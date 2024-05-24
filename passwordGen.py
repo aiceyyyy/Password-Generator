066ea6cf3f08956a1d1a1094d99b85813912d71e
Jakub Urbański <jakuburbanski2007@gmail.com>
2024-05-24 20:23:10 +0200

    Initialize project

---
+++ passwordGen.py
@@ -0,0 +1,41 @@
+import random
+import tools
+
+
+# User Interface
+print("Welcome to password generator!")
+user_letters_amount = int(input("How many letters do you want your password to have? "))
+user_special_characters_amount = int(input("How many special characters do you want your password to have? "))
+user_numbers_amount = int(input("How many numbers do you want your password to have? "))
+
+# Random password engine
+password_type = []
+password = ''
+
+for let in range(0, user_letters_amount):
+    password_type.append('letter')
+
+for char in range(0, user_special_characters_amount):
+    password_type.append('special_character')
+
+for num in range(0, user_numbers_amount):
+    password_type.append('number')
+
+# Shuffle chars types
+random.shuffle(password_type)
+
+for element in password_type:
+
+    random_letter_index = random.randint(0, len(tools.letters) - 1)
+    random_char_index = random.randint(0, len(tools.special_characters) - 1)
+    random_number_index = random.randint(0, len(tools.numbers) - 1)
+
+    if element == 'letter':
+        password += tools.letters[random_letter_index]
+    elif element == 'special_character':
+        password += tools.special_characters[random_number_index]
+    elif element == 'number':
+        password += tools.numbers[random_number_index]
+
+# Result
+print(f"Your password is: {password}")
